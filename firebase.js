7ff76ad8b1a91b55b1af97a244d08f2b13e7c999
Noor_Mohammad <noor.jsdivs@gmail.com>
2022-05-05 03:47:26 +0400

    google auth done

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7dsuDGQ_EhGMdFGADK0hTfIf6S_3YY7k",
+  authDomain: "clone-336eb.firebaseapp.com",
+  projectId: "clone-336eb",
+  storageBucket: "clone-336eb.appspot.com",
+  messagingSenderId: "517916459344",
+  appId: "1:517916459344:web:ca67a074ba85230b5cac55",
+};
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+// import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      key: process.env.key,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // GithubProvider({
+    //   key: process.env.key,
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
+    // ...add more providers here
+  ],
+});
